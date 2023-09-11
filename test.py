cb32492f50a2c1c59c03fecc46fb4191d855a652
kwenus <kwenus82@gmail.com>
2023-09-11 21:44:49 +0300

    уточнил профессию в test.py

--- test.py
+++ test.py
@@ -3,3 +3,11 @@ print('Hi, stranger!')
 name = input('What is your name?     ')
 
 print(f'Hello, {name}')
+
+prof = input('Who do you work here?    ')
+
+print(f'How much does {prof} earn?')
+
+salary = int(input())
+
+
